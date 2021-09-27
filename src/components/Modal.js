34c2399ea897a7d1016f50d8144461de89e2b464
Ogzhnsfgl <o.sofuoglu@yahoo.com>
2021-09-27 23:55:52 +0300

    Modal can be closed clicking on blank area now.

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -7,32 +7,41 @@ const Modal = (props) => {
   const { title, duration, author, embedLink } = props.item;
   const { setShowModal, showModal, stars, setStars } = props;
 
+  const handleModalClose = (e) => {
+    if (e.target.className == 'modal-wrapper') {
+      setShowModal(!showModal);
+    }
+  };
   return (
     <ReactModal
       className="ReactModal"
+      id="modal"
       isOpen={showModal}
-      contentLabel="Minimal Modal Example"
+      contentLabel="Higher Video Detail"
       ariaHideApp={false}
+      preventScroll={true}
     >
-      <div className="modal-info">
-        <div className="modal-info-title">{title}</div>
-        <div className="modal-info-author">{author}</div>
-        <div className="modal-info-detail">
-          <div className="modal-info-duration">Duration: {duration} </div>
-          <Rating item={props.item} stars={stars} setStars={setStars} />
+      <div className="modal-wrapper" onClick={handleModalClose}>
+        <div className="modal-info">
+          <div className="modal-info-title">{title}</div>
+          <div className="modal-info-author">{author}</div>
+          <div className="modal-info-detail">
+            <div className="modal-info-duration">Duration: {duration} </div>
+            <Rating item={props.item} stars={stars} setStars={setStars} />
+          </div>
         </div>
+        <div className="modal-video">
+          <iframe
+            width="500"
+            height="315"
+            src={`${embedLink}`}
+            title={`${title}`}
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <button onClick={() => setShowModal(!showModal)}>X</button>
       </div>
-      <div className="modal-video">
-        <iframe
-          width="500"
-          height="315"
-          src={`${embedLink}`}
-          title={`${title}`}
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
-      </div>
-      <button onClick={() => setShowModal(!showModal)}>X</button>
     </ReactModal>
   );
 };
